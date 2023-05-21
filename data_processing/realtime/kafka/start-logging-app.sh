2c46478122202931cf0dd8b1089d57944b036915
phuquanquan <tranphuquan0101@gmail.com>
2023-05-21 17:32:49 +0700

    Update data_processing

--- data_processing/realtime/kafka/start-logging-app.sh
+++ data_processing/realtime/kafka/start-logging-app.sh
@@ -1,4 +1,4 @@
 # start the application that is logging information
 # shellcheck disable=SC2164
 cd logging-app
-java -jar target/logging-app-1.0-SNAPSHOT.jar /home/cloudera/Downloads/log20160930.csv
+java -jar target/logging-app-1.0-SNAPSHOT.jar /Users/tpquan/Downloads/log20160930.csv
